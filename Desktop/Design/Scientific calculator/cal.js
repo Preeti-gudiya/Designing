e623d3b9a915e1accd8175b43c070f058a8dd3a0
Preeti-Gudiya <funkypreeti20@gmail.com>
2020-07-26 12:24:16 +0530

    My first commits in this folder

---
+++ Desktop/Design/Scientific calculator/cal.js
@@ -0,0 +1,68 @@
+function insertOp0()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var sqt=Math.sqrt(a);
+    alert(sqt);
+}
+
+function insertOp1()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var b=parseInt(document.getElementById("b1").value);
+    var p=Math.pow(a,b);
+    alert(p);
+}
+
+
+function insertOp2()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var b=parseInt(document.getElementById("b1").value);
+    var div=a/b;
+    alert(div);
+}
+
+function insertOp3()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var b=parseInt(document.getElementById("b1").value);
+    var mul=a*b;
+    alert(mul);
+}
+
+function insertOp4()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var b=parseInt(document.getElementById("b1").value);
+    var sub=a-b;
+    alert(sub);
+}
+
+function insertOp5()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var b=parseInt(document.getElementById("b1").value);
+    var sum=a+b;
+    alert(sum);
+}
+
+function insertOp6()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var s=Math.sin(a);
+    alert(s);
+}
+
+function insertOp7()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var c=Math.cos(a);
+    alert(c);
+}
+
+function insertOp8()
+{
+    var a=parseInt(document.getElementById("a1").value);
+    var t=Math.tan(a);
+    alert(t);
+}
